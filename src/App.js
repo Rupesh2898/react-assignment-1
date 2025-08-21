38b196b3b2a5ecf457da87abf2399dfa2baf790b
Rupesh Kumar Sharma <rusharma@gammastack.com>
2025-08-21 18:31:15 +0530

    Fix title in App component to reflect correct assignment number

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import FunctionCounter from './FunctionCounter';
 function App() {
   return (
     <div className="app-container">
-      <h1>Assignment-2 Counter Application</h1>
+      <h1>Assignment-1 Counter Application</h1>
       <div className="counter-row">
         <ClassCounter />
         <FunctionCounter />
